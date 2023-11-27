faaccf7fb72f4faba9d6f67fa1f9090ed41c7e51
Климентов Марк Алексеевич <klimentov51@mail.ru>
2023-11-27 10:16:36 +0300

    основной массив, фильтр

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-project';
+  title = 'todo';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    {description: 'dota game', done: true},
+    {description: 'training', done: true},
+    {description: 'eat', done: true},
+    {description: 'sleep', done: false}
+  ]
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems
+    }
+    return  this.allItems.filter(item => this.filter ? item.done : !item.done) 
+  }
+
+  
 }
