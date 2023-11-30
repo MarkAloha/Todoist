4d52a8bdcb76863e817cfdcdee0bc31b780ddb29
Климентов Марк Алексеевич <klimentov51@mail.ru>
2023-11-30 11:43:23 +0300

    модальное окно 3

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,12 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering(),
+    provideAnimations()
   ]
 };
 
