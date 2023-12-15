0896b3aa4f38774e76f3c1deac55aed3dc0a0590
Марк Климентов <klimentov51@mail.ru>
2023-12-16 00:41:29 +0300

    Роутинг модуль

---
+++ src/app/routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { TableComponent } from "./table/table.component";
+import { AuthorizationComponent } from "./authorization/authorization.component";
+
+// http://localhost:4200/ -> Home
+// http://localhost:4200/auth -> Auth
+
+
+const routes: Routes = [
+    {path: '', component: TableComponent},
+    {path: 'auth', component: AuthorizationComponent}
+
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+
+}
