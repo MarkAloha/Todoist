a8d1fad5c3df82ddd30c83c980b8a4a4c9a03c4b
Марк Климентов <klimentov51@mail.ru>
2023-12-18 17:56:10 +0300

    Компонент "не найдено" при вводе ошибочного адреса

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { TableComponent } from "./table/table.component";
 import { AuthorizationComponent } from "./authorization/authorization.component";
+import { NotFoundComponentComponent } from "./not-found.component/not-found.component.component";
 
 export const routes: Routes = [
     {path: '', component: TableComponent, pathMatch: 'full'},
-    {path: 'auth', component: AuthorizationComponent}
+    {path: 'auth', component: AuthorizationComponent},
+    {path: '**', component: NotFoundComponentComponent},
+
 
 ]
 
