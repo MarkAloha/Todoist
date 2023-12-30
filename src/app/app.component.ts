024bbd03d90976e2557a93185e8128233c1bb8ef
Марк Климентов <klimentov51@mail.ru>
2023-12-30 03:35:14 +0300

    Пайп с локализацией даты в таблице

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,71 +1,49 @@
-import { Component,Injectable } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
 import { RouterModule, RouterOutlet, Router } from '@angular/router';
-import { FormsModule, NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { TableModule } from 'primeng/table'
 import { TableComponent } from './table/table.component';
 import { TaskService } from './services/task.service';
 import { RegistrationComponent } from './registration.component/registration.component';
+import { PrimeNGConfig } from 'primeng/api';
+import { TranslateService } from '@ngx-translate/core';
+import '@angular/common/locales/global/ru'
+
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  providers: [TaskService, Router],
-  imports: [CommonModule, 
-    RouterOutlet, 
-    NgFor, 
-    FormsModule, 
-    TableModule, 
+  providers: [TaskService, Router, TranslateService, PrimeNGConfig],
+  imports: [CommonModule,
+    RouterOutlet,
+    NgFor,
+    FormsModule,
+    TableModule,
     TableComponent,
     RegistrationComponent,
     RouterModule,
-    RouterModule,
-     ],
-  // providers: [ ]
+    RouterModule,    
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
 
   constructor(
     private taskService: TaskService,
     private router: Router,
-  ) {}
+    private config: PrimeNGConfig,
+  ) {  }
 
-
-  title = 'todo';
-  done = true;
-
-  filter: 'all' | 'active' | 'done' = 'all';
-
-  // tasks!: Task[]
-
-  allItems = [
-    {id:1, description: 'dota game', done: true },
-    {id:2, description: 'training', done: true },
-    {id:3, description: 'eat', done: true },
-    {id:4, description: 'sleep', done: false },
-    {id:5, description: 'drink', done: false },
-  ];
-
-  get items() {
-    if (this.filter === 'all') {
-      return this.allItems;
-    }
-    return this.allItems.filter((item) =>
-      this.filter ? item.done : !item.done
-    );
-  } 
-
-  remove(item: any) {
-    this.allItems.splice(this.allItems.indexOf(item), 1);
+  ngOnInit() {
   }
 
-  callData = () => {
-    this.taskService.data
-  }
+
 
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -60,6 +60,7 @@ export class TableComponent implements OnDestroy {
   searchButton: boolean = false;
   searchStr: any = '';
   formGroup!: any
+  ru:any
 
   @ViewChild('showButton') showButton: any
   @ViewChild('changeButton') changeButton: any
@@ -74,6 +75,33 @@ export class TableComponent implements OnDestroy {
   ref: DynamicDialogRef | undefined;
 
   ngOnInit() {
+    this.ru = {
+      closeText: 'Закрыть',
+      prevText: 'Назад',
+      nextText: 'Вперёд',
+      monthNames: ['Январь', 'Февраль' , 'Март' , 'Апрель' , 'Май' , 'Июнь' , 'Июль' , 'Август' , 'Сентябрь','Октябрь','Ноябрь','Декабрь' ],
+      monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек' ],
+      dayNames: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+      dayNamesShort: ['Воск','Пон' , 'Вт' , 'Ср' , 'Четв' , 'Пят' , 'Суб'],
+      dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+      weekHeader: 'Неделя',
+      FirstDay: 1,
+      isRTL: false,
+      showMonthAfterYear: false,
+      yearSuffix:'',
+      timeOnlyTitle: 'Только время',
+      timeText: 'Время',
+      hourText: 'Час',
+      minuteText: 'Минута',
+      secondText: 'Секунда',
+      currentText: 'Сегодня',
+      ampm: false,
+      month: 'Месяц',
+      week: 'неделя',
+      day: 'День',
+      allDayText: 'Весь день'
+  }
+
     this.tasks = this.taskService.getTasksDataUser();
     this.formGroup = new FormGroup({
       date: new FormControl<Date | null>(null)
